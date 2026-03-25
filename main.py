74aec2a0ebafc13945f85c1d20c4fdfea596348c
Corey Michaud <cor3y.michaud@gmail.com>
2026-03-25 19:39:04 -0400

    Changed some formatting

--- main.py
+++ main.py
@@ -15,10 +15,15 @@ cursor.execute("SELECT * FROM companies")
 rows = cursor.fetchall()
 column_names = [description[0] for description in cursor.description]
 
-con.close()
-
 companies = [dict(zip(column_names, row)) for row in rows]
 # companies = companies[0:1]
+
+cursor.execute("SELECT * FROM seen")
+
+rows = cursor.fetchall()
+column_names = [description[0] for description in cursor.description]
+
+seen = [dict(zip(column_names, row)) for row in rows]
 
 print("[START] Beginning job scraper.")
 
@@ -38,6 +43,17 @@ for company in companies:
         # temp = module.scrape(company["company"], company["link"])
 
     found_jobs = found_jobs + temp
+
+for i in len(found_jobs):
+    if found_jobs[i] in seen:
+        found_jobs.remove(i)
+    else:
+        seen.append(found_jobs[i])
+
+
+# Figure out how to add the new jobs to the seen table in the database. I don't think the above code is even correct...
+
+con.close()
 
 notify(found_jobs)
 
