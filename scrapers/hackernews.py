2abd9f98d87d8ab248098f95e908c0210d56de5c
Corey Michaud <cor3y.michaud@gmail.com>
2026-04-01 14:48:15 -0400

    Ruff formatted

--- scrapers/hackernews.py
+++ scrapers/hackernews.py
@@ -20,7 +20,6 @@
 #             if await page.get_by_text("Who's Hiring?").count() == 1:
 #                 link = await page.get_by_text("Who's Hiring?").evaluate("el => el.href")
 
-            
 
 #         except TimeoutError:
 #             print(f"[SKIP] {company} timed out — skipping.")
@@ -28,11 +27,11 @@
 #         finally:
 #             await context.close()
 #             await browser.close()
-#      
+#
 #     return []
 
 # print(scrape("Hacker News", "https://news.ycombinator.com/item?id=47219668"))
 
 # COREY!!! I need to take time to code this... but the idea is to go to the new.ycombinator site and see if there is a whos hiring thread. if there is, then set that as the link to use and replace the current link on the db with that one.
-# Then go to the whos hiring thread and scrape the job posts. It should be that for each post, it will check if it has my keywords, and if it does then it will send it directly to discord with just the title and post, no link. 
+# Then go to the whos hiring thread and scrape the job posts. It should be that for each post, it will check if it has my keywords, and if it does then it will send it directly to discord with just the title and post, no link.
 # In the main.py file, it should not add any of the jobs from hacker news to the "found_jobs" list, since they will be sent directly to discord. So the scraper for hacker news should return an empty list, and just send the jobs directly to discord as it finds them.
